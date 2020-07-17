4eed958d83d5422bdd2841e0ac8869f47d02315d
Bruno Nascimento <bhenrique.contact@gmail.com>
2020-07-17 08:21:31 -0300

    Using macros to define a route

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{HttpServer, Responder, App, HttpResponse};
+use actix_web::get;
 
+#[get("/hello")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(index))
-            .route("/hello-again", web::get().to(index2))
+            .service(index)
+            // .route("/hello", web::get().to(index))
+            // .route("/hello-again", web::get().to(index2))
     })
     .bind("127.0.0.1:9000")?
     .run()
