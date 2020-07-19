b2fc49b092c6dda16880be59ef8958a3ff92ad00
Bruno Nascimento <bhenrique.contact@gmail.com>
2020-07-19 20:54:52 -0300

    serving static files

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
+use actix_files as fs;
 use actix_web::{App, HttpServer};
 mod routes;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(routes::index)
-        // App::new()
-        //     .handler(
-        //         '/',
-        //     )
-        // .route("/hello", web::get().to(index))
-        // .route("/hello-again", web::get().to(index2))
+        App::new()
+            .service(routes::index)
     })
     .bind("127.0.0.1:9000")?
     .run()
