0a11d74861b3689be047b0dc9d7118f13f963226
Bruno Nascimento <bhenrique.contact@gmail.com>
2020-07-19 21:24:53 -0300

    serving static files

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(routes::index)
+            .service(routes::serve_index_html)
+            .service(fs::Files::new("/", "./src/client"))
     })
     .bind("127.0.0.1:9000")?
     .run()
--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,13 @@
 use actix_web::get;
-use actix_web::{HttpResponse, Responder};
+use actix_web::{HttpResponse, Responder, Result};
+use actix_files::NamedFile;
 
 #[get("/hello")]
 pub async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/")]
+pub async fn serve_index_html() -> Result<NamedFile> {
+    Ok(NamedFile::open("./src/client/index.html")?)
 }
